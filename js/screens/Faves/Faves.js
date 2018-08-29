2db41745eb43b272413a7f2d17329fcff1596861
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-08-29 09:32:06 -0700

    finishing the fave page styling

--- js/screens/Faves/Faves.js
+++ js/screens/Faves/Faves.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, ScrollView, SectionList, TouchableOpacity } from "react-native";
 import moment from 'moment';
 import Icon from "react-native-vector-icons/Ionicons";
+import styles from './styles';
 
 const Faves = ({ sessions, navigation, faveIds}) => {
   return (
@@ -11,7 +12,7 @@ const Faves = ({ sessions, navigation, faveIds}) => {
         keyExtractor={item => item.id}
         renderSectionHeader={({section: {title}}) => (
           <View>
-            <Text>
+            <Text style={styles.time}>
               {moment(title).format('h:mm a')}
             </Text>
           </View>
@@ -22,14 +23,14 @@ const Faves = ({ sessions, navigation, faveIds}) => {
             onPress={() => navigation.navigate('Session', {itemId:item.id})}
           >
             <View>
-              <Text>{item.title}</Text>
+              <Text style={styles.title}>{item.title}</Text>
             </View>          
           </TouchableOpacity>
   
-          <View>
-            <Text>{item.location}</Text>
-            <Text>
-              { !faveIds.includes( item.id ) ? ( <Text /> ) : <Icon name='md-heart' size={20} color='red' /> }
+          <View style={styles.container}>
+            <Text style={styles.location}>{item.location}</Text>
+            <Text style={styles.icon}>
+              { !faveIds.includes( item.id ) ? ( <Text /> ) : <Icon name='md-heart' size={20} color='#cf392a' /> }
             </Text>
           </View>
         </View>
